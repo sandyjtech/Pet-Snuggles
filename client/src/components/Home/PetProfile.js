67431822551233909acee8ffcfa0676f289bef9d
sandyjtech <sandyjtech@gmail.com>
2023-08-27 10:21:00 -0700

    created all components skeleton

---
+++ client/src/components/Home/PetProfile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PetProfile = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PetProfile
---
+++ client/src/components/Schedule/AvailableTimes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AvailableTimes = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AvailableTimes
