3bb8d285e1bf513519feee656f71fcd9021b3723
sandyjtech <sandyjtech@gmail.com>
2023-08-30 07:27:46 -0700

    user auth contex

--- server/app.py
+++ server/app.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
-#app.py
+# app.py
 # Remote library imports
+from flask import request, session
+from sqlite3 import IntegrityError
 from flask import Flask, make_response, jsonify, request, session
-from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
 # Local imports
-from config import app, db, api
-# Add your model imports
+from config import app, db, Resource, api  # Import the Flask app and SQLAlchemy instance
 from models import User, Pet, Favorite, Schedule
 
-
+# Check Session
+class CheckSession(Resource):
+    def get(self):
+        if 'user_id' in session:  # Check if 'user_id' exists in session
+            user = User.query.get(session['user_id'])  # Use get() for primary key retrieval
+            if user:
+                return user.to_dict(), 200
+        return {'error': '401 Unauthorized'}, 401
 
 # Home Component
 class Pets(Resource):
@@ -17,35 +26,64 @@ class Pets(Resource):
         return make_response(pets, 200)
 
 # Protected route example using login_required decorator
-class Profile(Resource):
-    @login_required
-    def get(self):
-        user_id = session.get('user_id')
-        user = User.query.get_or_404(user_id)
-        return make_response(user.to_dict(), 200)
+class Signup(Resource):    
+    def post(self):
+        request_json = request.get_json()
+        username = request_json.get('username')
+        password = request_json.get('password')
+        address = request_json.get('address')
+        small_kids = request_json.get('small_kids')
+        own_pets = request_json.get('own_pets')
+        space = request_json.get('space')
+       
+        user = User(
+            username=username,
+            address=address,
+            small_kids=small_kids,
+            own_pets=own_pets,
+            space=space,
+        )
+        # the setter will encrypt this
+        user.password_hash = password
+        print('first')
+        try:
+            print('here!')
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            print(user.to_dict())
+            return user.to_dict(), 201
+        except IntegrityError:
 
-# Login route
-@app.route('/login', methods=['POST'])
-def login_route():
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-    if login(username, password):
-        return jsonify({'message': 'Logged in successfully'}), 200
-    return jsonify({'message': 'Invalid credentials'}), 401
+            print('no, here!')          
+            return {'error': '422 Unprocessable Entity'}, 422
 
-# Logout route
-@app.route('/logout', methods=['POST'])
-@login_required
-def logout_route():
-    logout()
-    return jsonify({'message': 'Logged out successfully'}), 200
+class Login(Resource):
+    def post(self):
+        request_json = request.get_json()
+        username = request_json.get('username')
+        password = request_json.get('password')
+        user = User.query.filter(User.username == username).first()
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+        return {'error': '401 Unauthorized'}, 401
 
-# Set up routes
+class Logout(Resource):
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {}, 204
+        return {'error': '401 Unauthorized'}, 401
+
+# Add the resources to the API
 api.add_resource(Pets, '/pets')
-api.add_resource(Profile, '/profile')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
+# Run the app if executed directly
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
-
--- server/config.py
+++ server/config.py
@@ -3,13 +3,13 @@
 
 # Remote library imports
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
@@ -25,9 +25,8 @@ db = SQLAlchemy(metadata=metadata)
 db.init_app(app)
 migrate = Migrate(app, db)
 
-
 # Instantiate REST API
 api = Api(app)
-
+bcrypt = Bcrypt(app)
 # Instantiate CORS
 CORS(app)
