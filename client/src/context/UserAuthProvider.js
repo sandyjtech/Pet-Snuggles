e528aa4c50d0674cc076ff9e88ffd9f4c79c4de8
sandyjtech <129788051+sandyjtech@users.noreply.github.com>
2023-08-30 15:21:03 -0700

    Update UserAuthProvider.js

--- client/src/context/UserAuthProvider.js
+++ client/src/context/UserAuthProvider.js
@@ -1,46 +1,44 @@
+// UserAuthProvider.js
 import React, { createContext, useContext, useState } from 'react';
 
-// Create a context for user authentication
 const UserAuthContext = createContext();
 
-// Custom hook to access the UserAuthContext
 export const useUserAuth = () => {
   return useContext(UserAuthContext);
 };
 
-// User authentication provider component
 const UserAuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
   const [signUp, setSignUp] = useState(false);
 
-  // Function to handle user login and signup
-  const handleLoginSubmit = async (values, actions) => {
+  const handleAuthSubmit = async (values, actions, authType) => {
     try {
-      // Make API call to your backend for login or signup
-      const response = await fetch('/login', {
+      // Determine the endpoint based on authType
+      const endpoint = authType === 'signup' ? '/signup' : '/login';
+
+      const response = await fetch(`${endpoint}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(values), // values contain username and password
+        body: JSON.stringify(values),
       });
-
+      console.log(endpoint)
       if (response.ok) {
         const userData = await response.json();
         setUser(userData);
       } else {
-        setError('Invalid username or password');
+        setError('Authentication error'); 
       }
     } catch (error) {
       setError('An error occurred');
     }
+    
   };
 
-  // Function to handle user logout
   const handleLogout = async () => {
     try {
-      // Make API call to your backend for logout
       await fetch('/logout', {
         method: 'DELETE',
       });
@@ -51,18 +49,16 @@ const UserAuthProvider = ({ children }) => {
     }
   };
 
-  // Function to toggle between login and signup
   const handleClick = () => {
     setSignUp(!signUp);
     setError(null);
   };
 
-  // Context value to provide to consuming components
   const contextValue = {
     user,
     error,
     signUp,
-    handleLoginSubmit,
+    handleAuthSubmit,
     handleLogout,
     handleClick,
   };
