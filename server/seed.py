b54c803876f6e264f78bcf05f081d699cc4111aa
sandyjtech <129788051+sandyjtech@users.noreply.github.com>
2023-08-30 15:16:07 -0700

    Update seed.py

--- server/seed.py
+++ server/seed.py
@@ -14,8 +14,7 @@ fake = Faker()
 with open('db.json', 'r') as json_file:
     data = json.load(json_file)
 
-dogs = data.get('dogs', [])
-cats = data.get('cats', [])
+# No need to load pet data since you're not reseeding pets
 
 def seed_users(num_users):
     for _ in range(num_users):
@@ -30,39 +29,19 @@ def seed_users(num_users):
         db.session.add(user)
     db.session.commit()
 
-def seed_pets(pet_data):
-    for animal_type, pet_info_list in pet_data.items():
-        for pet_info in pet_info_list:
-            pet = Pet(
-                animal_type=animal_type,
-                name=pet_info['name'],
-                age=pet_info['age'],
-                breed=pet_info['breed'],
-                sex=pet_info['sex'],
-                size=pet_info['size'],
-                temperament=pet_info['temperament'],
-                good_wt_pets=pet_info['good_wt_pets'],
-                good_wt_kids=pet_info['good_wt_kids'],
-                image=pet_info['image'],
-                adoption_link=pet_info['adoption_link']
-            )
-            db.session.add(pet)
-    db.session.commit()
+# No need for the seed_pets function
 
 if __name__ == "__main__":
     with app.app_context():
         print("Starting seed...")
 
-        # Clear existing data from the database
-        print("Clearing data...")
-        db.drop_all()
-        db.create_all()
+        # Clear existing user data from the database
+        print("Clearing user data...")
+        User.query.delete()
+        db.session.commit()
 
-        # Seed users
-        num_users = 5
-        ##seed_users(num_users)
-
-        # Seed pets
-        ##seed_pets({'dog': dogs, 'cat': cats})
+        # # Seed users
+        # num_users = 5
+        # seed_users(num_users)
 
         print("Seed complete!")
