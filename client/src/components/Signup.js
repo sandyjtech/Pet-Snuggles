8b119b80f3d784fccd466a0fbf16640e67a3d198
sandyjtech <sandyjtech@gmail.com>
2023-08-30 12:56:40 -0700

    added auth

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,66 +1,95 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useUserAuth } from "../context/UserAuthProvider";
+
 
 function Signup() {
-  const initialValues = {
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  };
-
-  const validationSchema = Yup.object({
-    username: Yup.string().required('Username is required'),
-    email: Yup.string().email('Invalid email format').required('Email is required'),
-    password: Yup.string().required('Password is required'),
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Passwords must match')
-      .required('Confirm password is required'),
-  });
-
-  const handleSubmit = (values, { setSubmitting }) => {
-    setSubmitting(true);
-    // Handle the signup logic here, e.g. making an API request
-    // to register the user with the provided information
-  };
+  const { signUp, handleAuthSubmit, handleClick, error } = useUserAuth();
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <div>
-            <label htmlFor="username">Username</label>
-            <Field type="text" id="username" name="username" />
-            <ErrorMessage name="username" />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <Field type="email" id="email" name="email" />
-            <ErrorMessage name="email" />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <Field type="password" id="password" name="password" />
-            <ErrorMessage name="password" />
-          </div>
-          <div>
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <Field type="password" id="confirmPassword" name="confirmPassword" />
-            <ErrorMessage name="confirmPassword" />
-          </div>
-          <div>
-            <button type="submit" disabled={isSubmitting}>
-              {isSubmitting ? 'Signing up...' : 'Sign Up'}
-            </button>
-          </div>
-        </Form>
-      )}
-    </Formik>
+    <div>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <h2>Please Sign Up!</h2>
+      <p>
+        Already a member? <span onClick={handleClick}>Log In!</span>
+      </p>
+      <Formik
+        initialValues={{
+          username: "",
+          password: "",
+          address: "",
+          small_kids: false,
+          own_pets: false,
+          space: "",
+          email: "", // Add this if you want to match your model structure
+        }}
+        validationSchema={Yup.object({
+          username: Yup.string().required("Username is required"),
+          password: Yup.string().required("Password is required"),
+          address: Yup.string().required("Address is required"),
+          small_kids: Yup.boolean(),
+          own_pets: Yup.boolean(),
+          space: Yup.string(),
+          email: signUp
+            ? Yup.string().email("Invalid email address").required("Email is required")
+            : Yup.string(),
+        })}
+        onSubmit={(values, actions) => {
+          handleAuthSubmit(values, actions, "signup"); // Specify action type as 'signup'
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <label htmlFor="username">Username</label>
+              <Field type="text" id="username" name="username" />
+              <ErrorMessage name="username" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                autoComplete="current-password"
+              />
+              <ErrorMessage name="password" />
+            </div>
+            <div>
+              <label htmlFor="address">Address</label>
+              <Field type="text" id="address" name="address" />
+              <ErrorMessage name="address" />
+            </div>
+            <div>
+              <label htmlFor="small_kids">Small Kids</label>
+              <Field type="checkbox" id="small_kids" name="small_kids" />
+            </div>
+            <div>
+              <label htmlFor="own_pets">Own Pets</label>
+              <Field type="checkbox" id="own_pets" name="own_pets" />
+            </div>
+            <div>
+              <label htmlFor="space">Space</label>
+              <Field type="text" id="space" name="space" />
+              <ErrorMessage name="space" />
+            </div>
+            {signUp && (
+              <div>
+                <label htmlFor="email">Email</label>
+                <Field type="text" id="email" name="email" />
+                <ErrorMessage name="email" />
+              </div>
+            )}
+            <div>
+              <button type="submit" disabled={isSubmitting}>
+                {isSubmitting ? "Loading..." : "Sign Up"}
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
 
