e29a8d60ec63c42b0bd0ec47391276648bf3eed8
sandyjtech <sandyjtech@gmail.com>
2023-08-29 17:22:03 -0700

    created login, signup, logout, and added authorizations

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,118 +1,79 @@
-import React, { useState } from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import IconButton from '@mui/material/IconButton';
-import InputLabel from '@mui/material/InputLabel';
-import OutlinedInput from '@mui/material/OutlinedInput'
-import InputAdornment from '@mui/material/InputAdornment';
-import TextField from '@mui/material/TextField';
-import FormControl from '@mui/material/FormControl';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-const initialFormState = {
-  username: "",
-  password: "",
-}
-export function InputAdornments() {
-  const [showPassword, setShowPassword] = React.useState(false);
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (e) => {
-    e.preventDefault();
+function Login({ onLogin }) {
+  const initialValues = {
+    username: "",
+    password: "",
   };
-  return (
-    <div>
-          <img src={process.env.PUBLIC_URL + "Dogoutline.png"} alt="PetSnuggles logo" width="150px" />
-          <h1>Pet Snuggles</h1>
-    <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-      <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-      <OutlinedInput
-        id="outlined-adornment-password"
-        type={showPassword ? 'text' : 'password'}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              onClick={handleClickShowPassword}
-              onMouseDown={handleMouseDownPassword}
-              edge="end"
-            >
-              {showPassword ? <VisibilityOff /> : <Visibility />}
-            </IconButton>
-          </InputAdornment>
-        }
-        label="Password"
-      />
-    </FormControl>
-  </div>
-);
 
+  const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const handleSubmit = (values, { setSubmitting, setErrors }) => {
+    setSubmitting(true);
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((data) => {
+            setErrors(data.errors);
+          });
+        }
+      })
+      .then((user) => {
+        onLogin(user);
+      })
+      .catch((error) => {
+        console.error("Login error:", error);
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <label htmlFor="username">Username</label>
+            <Field type="text" id="username" name="username" />
+            <ErrorMessage name="username" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <Field
+              type="password"
+              id="password"
+              name="password"
+              autoComplete="current-password"
+            />
+            <ErrorMessage name="password" />
+          </div>
+          <div>
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Loading..." : "Login"}
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
 }
 
-
-export function Login() {
-  const [form, setForm] = useState (initialFormState)
-  // const options = parks ? parks.map((park) => (
-  //   { key: park.LocationNumber, text: park.LocationName, value: park.LocationName }
-  // )) : [];
-     function handleForm(e, data) {
-      setForm({...form, [data.name] : data.value})
-     }
-    
-        function handleSubmit(e){
-          e.preventDefault()
-          fetch('', {
-            method: '',
-            headers: { 'Content-Type' : 'application/json'},
-            body: JSON.stringify({
-              'name' : form.name,
-              'password' : form.password
-            })
-        })
-        .then(res =>res.json())
-        setForm(initialFormState)
-        }
-  
-    return (
-        <React.Fragment>
-          <CssBaseline />
-          <Container></Container>
-          <Container fixed>
-            <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-              <div>
-                <TextField
-                  label="username"
-                  id="outlined-start-adornment"
-                  sx={{ m: 1, width: '25ch' }}
-                  InputProps={{
-                    startAdornment: <InputAdornment position="start">username</InputAdornment>,
-                  }}
-                  onChange={(e) => handleForm(e, { name: 'username', value: e.target.value })}
-                />
-                <InputAdornments /> 
-              </div>
-              <Stack spacing={2} direction="row">
-                <Button variant="outlined" onClick={handleSubmit}>Login</Button>
-              </Stack>
-            </Box>
-            <div>
-                <Stack spacing={2} direction="row">
-                  <Button variant="outlined">Sign up</Button>
-                </Stack>
-            </div>
-            <div>
-              <Stack spacing={2} direction="row">
-                <Button variant="outlined">forgot password</Button>
-              </Stack>
-            </div>
-          </Container>
-        </React.Fragment>
-      );
-    }
-
-export default Login
+export default Login;
--- client/src/context/UserAuthProvider.js
+++ client/src/context/UserAuthProvider.js
@@ -1,3 +1,35 @@
+import React, { createContext, useState } from 'react';
 
+export const UserAuthContext = createContext();
 
+export function UserAuthProvider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
 
+  const login = async (username, password) => {
+    try {
+      // Your login logic here...
+      // Make API request, update isAuthenticated and user state, etc.
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    setUser(null);
+  };
+
+  const contextValue = {
+    isAuthenticated,
+    user,
+    login,
+    logout,
+  };
+
+  return (
+    <UserAuthContext.Provider value={contextValue}>
+      {children}
+    </UserAuthContext.Provider>
+  );
+}
