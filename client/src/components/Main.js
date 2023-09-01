cde4bba0ed38e1270822e283e6baac93a33903fd
sandyjtech <129788051+sandyjtech@users.noreply.github.com>
2023-09-01 07:41:00 -0700

    Update Main.js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,22 +3,21 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomeContainer from './Home/HomeContainer';
 import PetProfile from './PetProfile';
-import ProfileContainer from './Profile/ProfileContainer';
+import ProfileContainer from './Profile/ProfileContainer'; 
 import ScheduleContainer from './Schedule/ScheduleContainer';
 import FavoritesContainer from './Favorite/FavoritesContainer';
 
 export default function Main() {
-  
+
   return (
     <div>
       <Switch>
-        <Route path="/pets/:id" ><PetProfile/> </Route>
-        <Route path="/profile/:id" ><ProfileContainer/> </Route>
-        <Route path="/schedule/:id" ><ScheduleContainer/></Route>        
-        <Route path="/favorites/:id" ><FavoritesContainer/></Route>
+        <Route path="/pets/:id" ><PetProfile/></Route>
+        <Route path="/profile" ><ProfileContainer/></Route>         
+        <Route path="/schedule" ><ScheduleContainer/></Route>        
+        <Route path="/favorites" ><FavoritesContainer/></Route>
         <Route path="/"><HomeContainer /></Route>
       </Switch>
     </div>
   );
 }
-
